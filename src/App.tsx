0fe7ffeaa8d71b5944dff621e74dde23f1d2274d
SanthoshNumentica <santhoshnumentica@gmail.com>
2025-01-12 03:41:33 +0530

    skilll

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Experience } from './components/Experience';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { Projects } from './components/Projects';
-import Skills from './components/skills';
+import Skills from './Skill';
 
 function App() {
   return (
